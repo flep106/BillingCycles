8d14ec99e580225df822b327d72b23aab42fade6
flep106 <vandrilho@gmail.com>
2019-05-02 19:27:03 -0300

    atualizado

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -2,5 +2,11 @@ const BillingCycle = require('./billingCycle')
 
 //vai criar os metodos da API REST  padrao 
 BillingCycle.methods(['get','post','put','delete'])
+//Sempre que der o update, retorne o objecto novo, nao o antigo
+BillingCycle.updateOptions({new: true, runValidators: true})
+
+BillingCycle.route('count', function(req, res, next){
+    BillingCycle.count()
+})
 
 module.exports = BillingCycle
--- config/database.js
+++ config/database.js
@@ -3,3 +3,5 @@ const mongoose = require('mongoose')
 module.exports = mongoose.connect('mongodb://localhost/dbfinance')
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor limite {MIN}."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior limite {MAX}."
